6c22333a449c884dded299b144873adb86811e94
amantiwaricr <amanbkvm2019@gmail.com>
2024-01-19 11:52:25 +0545

    aman

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 my name is aman
 
 aman is my name
+gfrdrc
---
+++ a.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+ 
+ int main()
+
+ {
+
+    int a;
+    printf("enter the number:");
+    scanf("%d",&a);
+    a%2==0?printf("even"):printf("odd");
+    
+ }
